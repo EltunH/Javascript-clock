a153d16b2b86e7e3641f20602779ceb83eb6464f
EltunH <eltun.huseynov17@gmail.com>
2025-01-17 17:51:20 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const hoursDiv = document.querySelector('#hoursDiv')
+const minutesDiv = document.querySelector('#minutesDiv')
+const secondsDiv = document.querySelector('#secondsDiv')
+
+function clock() {
+    let date = new Date()
+    hoursDiv.innerHTML = date.getHours()
+    minutesDiv.innerHTML = `${date.getMinutes()}`.padStart(2, 0)
+    secondsDiv.innerHTML = `${date.getSeconds()}`.padStart(2, 0)
+    console.log('as');
+}
+
+setInterval(clock, 1000)
